0af6ee59b9387b644c9078891cb70365b0947e13
X60022012X <leo.fredenborg@gmail.com>
2025-05-05 21:46:15 +0200

    1

--- new attempt/key_expan.py
+++ new attempt/key_expan.py
@@ -1,18 +1,52 @@
 from pass_expan import expand_password
+from constants import *
 
-first_key = expand_password("password")
-print(first_key)
-print(list(first_key))
-print(bytes(list(first_key)))
+#Rotate bytes i word
+def rot_word(word):
+     return word[1:] + word[:1]
 
-words = []
+#Substitute bytes in word
+def sub_word(temp):
+    word = bytearray()
+    for i in range(4):
+        word.append(S_BOX[temp[i]])
+    return bytes(word)
 
-for i in range(0, 32, 4):
-    word = first_key[i:i+4]
-    words.append(word)
+#XOR two byte objects
+def xor(temp1, temp2):
+    word = bytearray()
+    for i in range(4):
+        word.append(temp1[i] ^ temp2[i])
+    return bytes(word)
 
 
-print(first_key)
-print(len(list(first_key)))
+def expand_key(password):
+    initial_key = expand_password(password)
 
-print(words)
+    words = []
+    keys = []
+
+    #Transform initial key to words
+    for i in range(0, 32, 4):
+        word = initial_key[i:i+4]
+        words.append(word)
+
+    #Create remaining words
+    for i in range(8, 60):
+            temp = words[i - 1]
+
+            if i % 8 == 0:
+                temp = rot_word(temp)
+                temp = sub_word(temp)
+                temp = xor(temp, RCON[i // 8].to_bytes(4, 'big'))
+            elif i % 8 == 4:
+                temp = sub_word(rot_word(temp)) 
+
+            word = xor(temp, words[i - 8])
+            words.append(word)
+
+    #Transfrom from words to keys
+    for i in range(ROUNDS):
+        keys.append(b''.join(words[i*4:i*4+4]))
+
+    return keys
